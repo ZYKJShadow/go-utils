03213a73d19a177ada066522fa1dda62729bcc57
ZYKJShadow <857405578@qq.com>
2021-07-25 09:27:18 +0800

    update tcp method

--- tcp/network/tcp_conn.go
+++ tcp/network/tcp_conn.go
@@ -1,1 +1,107 @@
 package network
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"net"
+)
+
+type TCPClient struct {
+	conn net.Conn
+	r    *bufio.Reader
+	w    *bufio.Writer
+	head int32
+	tag  int32
+}
+
+func NewTCPClient(conn net.Conn, headLen int32, tagLen int32) *TCPClient {
+	return &TCPClient{
+		conn: conn,
+		r:    bufio.NewReader(conn),
+		w:    bufio.NewWriter(conn),
+		head: headLen,
+		tag:  tagLen,
+	}
+}
+
+func (receiver *TCPClient) LocalAddr() net.Addr {
+	return receiver.conn.LocalAddr()
+}
+
+func (receiver *TCPClient) RemoteAddr() net.Addr {
+	return receiver.conn.RemoteAddr()
+}
+
+func (receiver *TCPClient) Close() error {
+	return receiver.conn.Close()
+}
+
+func (receiver *TCPClient) Write(message []byte, tag int32) (int, error) {
+	// 读取消息长度
+	var length = int32(len(message))
+	var pkg = new(bytes.Buffer)
+	// 写入消息头
+	err := binary.Write(pkg, binary.BigEndian, length)
+	if err != nil {
+		return 0, err
+	}
+	// 写入消息类型
+	err = binary.Write(pkg, binary.BigEndian, tag)
+	if err != nil {
+		return 0, err
+	}
+	// 写入消息体
+	err = binary.Write(pkg, binary.BigEndian, message)
+	if err != nil {
+		return 0, err
+	}
+	write, err := receiver.w.Write(pkg.Bytes())
+	if err != nil {
+		return 0, err
+	}
+
+	err = receiver.w.Flush()
+	if err != nil {
+		return 0, err
+	}
+	return write, nil
+}
+
+func (receiver *TCPClient) Read() (int32, []byte, error) {
+	// Peek返回缓存的一个切片，该切片引用缓存中前N个字节的数据
+	lengthByte, err := receiver.r.Peek(int(receiver.head + receiver.tag))
+	if err != nil {
+		return 0, nil, err
+	}
+	// 创建buffer缓冲器
+	var length int32
+	lengthBuff := bytes.NewBuffer(lengthByte[:receiver.head])
+	// 通过Read接口可以将buff中的内容填充到data参数表示的数据结构中
+	err = binary.Read(lengthBuff, binary.BigEndian, &length)
+	if err != nil {
+		return 0, nil, err
+	}
+	var tag int32
+	tagBuff := bytes.NewBuffer(lengthByte[receiver.head:])
+	err = binary.Read(tagBuff, binary.BigEndian, &tag)
+	if err != nil {
+		return 0, nil, err
+	}
+	// Buffered 返回缓存中未读取的数据长度
+	if int32(receiver.r.Buffered()) < length+receiver.head+receiver.tag {
+		return 0, nil, err
+	}
+	// 读取消息真正的内容
+	pack := make([]byte, int(receiver.head+length+receiver.tag))
+	// Read从 b 中读出数据到 p 中，返回读出的字节数和遇到的错误。
+	// 如果缓存不为空，只能读出缓存中的数据，不会从底层io.Reader中提取数据
+	// 如果缓存为空
+	// 1、len(p) >= 缓存大小，则跳过缓存，直接从底层io.Reader中读出到p
+	// 2、len(p) < 缓存大小，则先将数据从底层io.Reader读到缓存中，再从缓存读到p中
+	_, err = receiver.r.Read(pack)
+	if err != nil {
+		return 0, nil, err
+	}
+	return tag, pack[receiver.head+receiver.tag:], nil
+}
--- tcp/network/tcp_server.go
+++ tcp/network/tcp_server.go
@@ -1,1 +1,64 @@
 package network
+
+import (
+	"fmt"
+	"github.com/prometheus/common/log"
+	"net"
+	"time"
+)
+
+func InitTcp(address string, headLen, tagLen int32) {
+	addr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		log.Error(err)
+	}
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		log.Error(err)
+	}
+	go acceptTcp(listener, headLen, tagLen)
+}
+
+func acceptTcp(listener *net.TCPListener, headLen, tagLen int32) {
+	for {
+		var (
+			conn *net.TCPConn
+			err  error
+		)
+		if conn, err = listener.AcceptTCP(); err != nil {
+			log.Error(err)
+			return
+		}
+		if err = conn.SetKeepAlive(false); err != nil {
+			log.Error(err)
+			return
+		}
+		if err = conn.SetReadBuffer(1024); err != nil {
+			log.Error(err)
+			return
+		}
+		if err = conn.SetWriteBuffer(1024); err != nil {
+			log.Error(err)
+			return
+		}
+		go serveTCP(conn, headLen, tagLen)
+	}
+}
+
+func serveTCP(conn *net.TCPConn, headLen, tagLen int32) {
+	client := NewTCPClient(conn, headLen, tagLen)
+	_ = client.conn.SetDeadline(time.Now().Add(time.Duration(10) * time.Second))
+	go func() {
+		for {
+			tag, data, err := client.Read()
+			if err != nil {
+				log.Error(err)
+				_ = client.Close()
+				return
+			}
+			_ = client.conn.SetDeadline(time.Now().Add(time.Duration(10) * time.Second))
+			fmt.Println("tag:", tag)
+			fmt.Println(string(data))
+		}
+	}()
+}
